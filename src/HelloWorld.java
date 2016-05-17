fed06c0a69f29ade50a0744aad6a26e8ff01cbde
stevenbian <groom_bian@hotmail.com>
2016-05-17 15:10:08 +1000

    add allan niubi3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
   public static void main(String[] args){
 System.out.println("hello world");
-System.out.println("Allan niubi");
+System.out.println("Allan bu niubi");
 System.exit(0);
 }
 }
